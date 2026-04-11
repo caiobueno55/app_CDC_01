961feef0c9d8905c737db7c2b549338e28690350
caiobueno55 <152219316+caiobueno55@users.noreply.github.com>
2026-04-10 21:22:45 -0300

    Remove commented code in App.js

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { estilos } from './src/styleSheet/style';
 import Conteudo from './src/Conteudo';
 
 export default function App() {
-  // Verifique se a imagem existe na pasta img
+ 
   const imgLogo = require("./img/logo2.png"); 
 
   return (
     <View style={estilos.fundo}>
-      {/* Cabeçalho montado diretamente para evitar erros de importação */}
+      {}
       <View style={estilos.cabecalho}>
         <Text style={estilos.cabSubTitulo}>DSV MOBILE</Text>
         <Image source={imgLogo} style={estilos.cabimagem} />
@@ -18,7 +18,7 @@ export default function App() {
 
       <Conteudo />
 
-      {/* Rodapé montado diretamente */}
+      {}
       <View style={estilos.rodape}>
         <Text style={estilos.rodTexto}>Caio Bueno</Text>
         <Text style={estilos.rodTexto}>10/04/2026</Text>
